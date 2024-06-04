f3b0b65961cd4799cffb9abefd2718e5979de0a2
priyank-saini <96546478+priyank-saini@users.noreply.github.com>
2024-06-04 22:37:11 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// import routes
+import userRouter from "./routes/user.routes.js"
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
 export default app;
